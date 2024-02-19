143ac86a11b4e95b63d7758cd4496068006c1d7e
Miljan Lazic <miljanlazic@gmail.com>
2024-02-19 19:37:26 +0100

    Feature: Navbar

--- src/ToolCard.jsx
+++ src/ToolCard.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const ToolCard = ({ imageUrl, toolName, toolDescription }) => {
 	return (
-		<div className="m-5 animate-fade-left border border-gray-300 animate-once">
-			<img src={imageUrl} alt={toolName} />
-			<h3>{toolName}</h3>
-			<p>{toolDescription}</p>
-			<button>Learn More</button>
+		<div className="m-5 flex h-56 w-56 transform  animate-fade-up flex-col items-center justify-center rounded-md border border-gray-300 duration-150 hover:scale-110">
+			{/* <img src={imageUrl} alt={toolName} /> */}
+			<h2 className=" text-center text-xl font-semibold">{toolName}</h2>
+			<p className="mt-5 text-center ">{toolDescription}</p>
+			<button className="mx-auto mt-20 h-10 w-3/4 rounded-3xl bg-black font-semibold text-white duration-300 hover:bg-white hover:text-black">
+				Learn More
+			</button>
 		</div>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import HomePage from "./HomePage.jsx";
+import Navbar from "./navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		{/* <App /> */}
+		<Navbar />
 		<HomePage />
 	</React.StrictMode>,
 );
