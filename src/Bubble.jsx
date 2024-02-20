950953bcf6c484348865a8e58c597e41cebb680e
Miljan Lazic <miljanlazic@gmail.com>
2024-02-20 18:15:42 +0100

    bugfix with on hover animation

--- src/Bubble.jsx
+++ src/Bubble.jsx
@@ -8,7 +8,7 @@ const Bubble = ({ title, icon: Icon, handleDragStart }) => {
 				onDragStart={(e) => handleDragStart(e, title)}
 				key={title}
 				draggable="true"
-				className="m-3 flex cursor-grab select-none flex-col items-center justify-center space-y-4 rounded-2xl border border-zinc-700 bg-zinc-800 bg-opacity-40 p-8 backdrop-blur-lg duration-100 active:cursor-grabbing active:outline-none"
+				className="m-3 flex cursor-grab select-none flex-col items-center justify-center space-y-4 rounded-2xl border border-zinc-700 bg-zinc-800 bg-opacity-40 p-8 backdrop-blur-lg duration-100 hover:scale-105 active:cursor-grabbing active:outline-none"
 			>
 				{Icon && <Icon className="text-2xl text-zinc-400" />}
 				<h1 className="text-md text-center font-thin text-zinc-400">
