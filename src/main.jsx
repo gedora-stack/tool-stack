328031d387eecac7f6bd6a363d90642dbbe98fd7
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2024-02-21 09:50:09 +0100

    Cleaned Up Repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import HomePage from "./HomePage.jsx";
-import Navbar from "./navbar.jsx";
-import MockupPage from "./MockupPage.jsx";
+import MockupPage from "./Components/MockupPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
