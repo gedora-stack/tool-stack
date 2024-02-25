e7e0361068091c6324f9ddd7e69385ad06331fbd
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2024-02-25 21:19:52 +0100

    Changed quick control styling

--- src/Components/BannerTool.jsx
+++ src/Components/BannerTool.jsx
@@ -4,15 +4,17 @@ import { BsBoxArrowUpRight, BsPlusSquare } from "react-icons/bs";
 
 const Styles = {
 	container:
-		"group relative flex h-36 w-56 max-w-56 cursor-grab select-none flex-col items-center justify-center space-y-4 rounded-2xl border border-zinc-700 bg-zinc-800 bg-opacity-40 p-8 backdrop-blur-lg duration-100 active:cursor-grabbing active:outline-none",
+		"group relative flex h-48 w-56 max-w-56 cursor-grab select-none flex-col items-center justify-center space-y-4 rounded-2xl border border-zinc-700 bg-zinc-800 bg-opacity-40 p-8 backdrop-blur-lg duration-100 active:cursor-grabbing active:outline-none",
 	description:
 		"absolute w-40 text-center text-sm font-thin text-zinc-500 opacity-0 duration-300 group-hover:translate-y-6 group-hover:opacity-100",
 	icon: "text-2xl text-zinc-400 duration-300 group-hover:opacity-0",
 	title: "text-md text-center font-thin text-zinc-400 duration-300 group-hover:-translate-y-14",
-	replaceIcon:
-		"absolute -top-1 right-3 text-zinc-600 duration-1000 opacity-0 group-hover:opacity-100 text-lg",
-	addIcon:
-		"absolute bottom-3 left-3 text-zinc-600 duration-1000 opacity-0 group-hover:opacity-100 text-lg",
+	replaceControl:
+		"absolute font-thin -top-1.5 flex flex-row items-center justify-center text-zinc-600 duration-1000 opacity-0 group-hover:opacity-100 text-lg",
+	addControl:
+		"absolute bottom-2 font-thin flex flex-row items-center justify-center text-zinc-600 duration-1000 opacity-0 group-hover:opacity-100 text-lg",
+	replaceIcon: "text-lg ml-2",
+	addIcon: "text-lg mr-2",
 };
 
 const BannerTool = ({
@@ -43,18 +45,24 @@ const BannerTool = ({
 					{Icon && <Icon className={Styles.icon} />}
 					<h1 className={Styles.title}>{title}</h1>
 				</div>
-				<BsBoxArrowUpRight
+				<div
 					onClick={() => {
 						handleSingleMode({ id: id, title: title });
 					}}
-					className={Styles.replaceIcon}
-				/>
-				<BsPlusSquare
+					className={Styles.replaceControl}
+				>
+					Single mode
+					<BsBoxArrowUpRight className={Styles.replaceIcon} />
+				</div>
+				<div
 					onClick={() => {
 						handleQuickAdd({ id: id, title: title });
 					}}
-					className={Styles.addIcon}
-				/>
+					className={Styles.addControl}
+				>
+					<BsPlusSquare className={Styles.addIcon} />
+					Quick add
+				</div>
 			</motion.div>
 		</AnimatePresence>
 	);
