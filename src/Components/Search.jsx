1890a0b53ef785d8561698432858f3edc61b2ac7
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2024-02-25 22:30:43 +0100

    Fix: Search on mobile & scaling

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Styles = {
-	search: "fixed z-10 w-[30%] min-w-96 appearance-none rounded-2xl bg-zinc-800 bg-opacity-40 px-4 py-2 font-thin text-zinc-400 caret-zinc-500 outline outline-1 outline-zinc-600 backdrop-blur-lg transition-all duration-300 placeholder:text-zinc-500 hover:scale-105 focus:outline-offset-0 lg:w-[55%] xl:w-[65%]",
+	search: "z-10 w-[30%] min-w-96 appearance-none rounded-2xl bg-zinc-800 bg-opacity-40 px-4 py-2 font-thin text-zinc-400 caret-zinc-500 outline outline-1 outline-zinc-600 backdrop-blur-lg transition-all duration-300 placeholder:text-zinc-500 hover:scale-105 focus:outline-offset-0 lg:w-[70%] xl:w-[80%]",
 };
 
 const Search = ({ onChange }) => {
